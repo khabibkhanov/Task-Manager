b706b7afcdc4906d0a8a338bbc988aa0b8171c1f
Khabibkhanov <hamidulloxon2002@gmail.com>
2025-12-14 14:54:09 +0500

    initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { GroupsModule } from './groups/groups.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, GroupsModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
